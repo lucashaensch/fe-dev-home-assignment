3a2e59d3beb764ec8654dc5ef42bd07132121300
lucashaensch <lucashaensch@gmail.com>
2015-05-15 19:24:53 -0300

    added json and started doing carousel

--- fe-dev-home-assignment/js.js
+++ fe-dev-home-assignment/js.js
@@ -1,1 +1,50 @@
 // you can enter your JS here!
+MINOR_VALUE = -10920
+MAJOR_VALUE = 0
+
+var outOfBoundaries = function(value){
+	if (value > 0 || value < -10920) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+var paginationDirection = function(value){
+	if (value > 0) {
+		return "left";
+	} else {
+		return "right";
+	}
+}
+
+$( ".next" ).click(function() {
+	$( ".carousel-list" ).css("left", function(index, currValue){
+		currValue = currValue.slice(0,-2);
+		if (outOfBoundaries(currValue)) {
+			if (paginationDirection == "left") {
+				return (MINOR_VALUE + "px");
+			} else {
+				return (MAJOR_VALUE + "px");
+			}
+		} else {
+			return ((currValue - 840) + "px");
+		}
+	});
+});
+
+$( ".prev" ).click(function() {
+	$( ".carousel-list" ).css("left", function(index, currValue){
+		currValue = currValue.slice(0,-2);
+		if (outOfBoundaries(currValue)) {
+			if (paginationDirection == "left") {
+				return (MINOR_VALUE + "px");
+			} else {
+				return (MAJOR_VALUE + "px");
+			}
+		} else {
+			return ((currValue + 840) + "px");
+		}
+	});
+});
+
